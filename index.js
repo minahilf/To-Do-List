ac143fbd40a90816a5d8c52e13fb85c00a52e37c
minahilfatima <sminahilfatima@gmail.com>
2024-04-03 17:32:08 +0500

    Added remove function and update function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 let todos = [];
 let condition = true;
@@ -7,6 +8,13 @@ while (condition) {
             name: "todo",
             type: "input",
             message: "What do you want to add in your Todos",
+            // Ramadan coding night 7 assignment 1 if user enter nothing...
+            validate: (input) => {
+                if (input.trim() === "") { // agr user ne kch nh add krega to ye msg print hoga
+                    return "Please add some text for the todo.";
+                }
+                return true;
+            },
         },
         {
             name: "addMoreTodos",
@@ -19,6 +27,7 @@ while (condition) {
     condition = addingTodos.addMoreTodos;
     console.log(todos);
 }
+// remove todos
 let removeTodos = await inquirer.prompt([
     {
         name: "removeTodo",
@@ -28,9 +37,49 @@ let removeTodos = await inquirer.prompt([
     },
 ]);
 if (removeTodos.removeTodo) {
-    todos.pop();
+    let removeTodoChoice = await inquirer.prompt([
+        {
+            name: "todoToRemove",
+            type: "list",
+            message: "Select a todo to remove:",
+            choices: todos,
+        },
+    ]);
+    const indexToRemove = todos.indexOf(removeTodoChoice.todoToRemove);
+    if (indexToRemove !== -1) {
+        todos.splice(indexToRemove, 1);
+    }
 }
+// assignment now you can update your todo if your spelling is incorrect
+let updateTodos = await inquirer.prompt([
+    {
+        name: "updateTodo",
+        type: "confirm",
+        message: "Do you want to update a Todo?",
+        default: "false",
+    },
+]);
+if (updateTodos.updateTodo) {
+    let updateTodoChoice = await inquirer.prompt([
+        {
+            name: "todoToUpdate",
+            type: "list",
+            message: "Select a todo to update:",
+            choices: todos,
+        },
+        {
+            name: "newTodo",
+            type: "input",
+            message: "Enter the new value for the selected todo:",
+        },
+    ]);
+    const indexToUpdate = todos.indexOf(updateTodoChoice.todoToUpdate);
+    if (indexToUpdate !== -1) {
+        todos[indexToUpdate] = updateTodoChoice.newTodo;
+    }
+}
+// Ramadan coding night 7 assignment 2 by sir ameen individually printing todos
 console.log("Your Todos:");
-for (let i = 0; i < todos.length; i++) {
-    console.log(todos[i]);
-}
+todos.forEach(todo => {
+    console.log("- " + todo);
+});
--- index.ts
+++ index.ts
@@ -5,11 +5,20 @@ let todos: string[] = [];
 let condition = true;
 
 while (condition) {
+ 
   let addingTodos = await inquirer.prompt([
     {
+      
       name: "todo",
       type: "input",
       message: "What do you want to add in your Todos",
+      // Ramadan coding night 7 assignment 1 if user enter nothing...
+      validate: (input) => { // it  ensure that the input provided meets certain criteria before proceeding
+        if (input.trim() === "") { // agr user ne kch nh add krega to ye msg print hoga
+          return "Please add some text for the todo.";
+        }
+        return true;
+      },
     },
     {
       name: "addMoreTodos",
@@ -24,6 +33,8 @@ while (condition) {
   console.log(todos);
 }
 
+// remove todos
+
 let removeTodos = await inquirer.prompt([
   {
     name: "removeTodo",
@@ -34,10 +45,55 @@ let removeTodos = await inquirer.prompt([
 ]);
 
 if(removeTodos.removeTodo){
-    todos.pop();
+  let removeTodoChoice = await inquirer.prompt([
+    {
+      name: "todoToRemove",
+      type: "list",
+      message: "Select a todo to remove:",
+      choices: todos,
+    },
+  ]);
+  const indexToRemove = todos.indexOf(removeTodoChoice.todoToRemove);
+  if (indexToRemove !== -1) {
+    todos.splice(indexToRemove, 1);
+  }
 }
 
-console.log("Your Todos:");
-for (let i = 0; i < todos.length; i++) {
-  console.log(todos[i]);
+// assignment now you can update your todo if your spelling is incorrect
+
+let updateTodos = await inquirer.prompt([
+  {
+    name: "updateTodo",
+    type: "confirm",
+    message: "Do you want to update a Todo?",
+    default: "false",
+  },
+]);
+
+if (updateTodos.updateTodo) {
+  let updateTodoChoice = await inquirer.prompt([
+    {
+      name: "todoToUpdate",
+      type: "list",
+      message: "Select a todo to update:",
+      choices: todos,
+    },
+    {
+      name: "newTodo",
+      type: "input",
+      message: "Enter the new value for the selected todo:",
+    },
+  ]);
+  const indexToUpdate = todos.indexOf(updateTodoChoice.todoToUpdate);
+  if (indexToUpdate !== -1) {
+    todos[indexToUpdate] = updateTodoChoice.newTodo;
+  }
 }
+
+
+// Ramadan coding night 7 assignment 2 by sir ameen individually printing todos
+console.log("Your Todos:");
+todos.forEach(todo => {
+  console.log("- " + todo);
+});
+
